d225ec77162a8cb0442abc5a9b369bb7dda597aa
Mil4gritos <106402724+Mil4gritos@users.noreply.github.com>
2023-02-08 23:30:11 +0100

    Delete proyectoImf directory

--- proyectoImf/layNotRegister.php
+++
@@ -1,9 +0,0 @@
-<?php require_once 'includes/headerNotRegister.php'; ?>
-<?php require_once 'includes/loginPost.php'; ?>
-
-<?php if(isset($_SESSION['usuario'])) : ?>
-<div id="" class="block">
-    <h2><?=$_SESSION['usuario']['nombre'].' '.$_SESSION['usuario']['apellidos'];?></h2>
-</div>
-<?php endif;?>
-<?php require_once 'includes/footer.php';?>
